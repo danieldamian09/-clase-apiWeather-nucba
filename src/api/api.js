b13bae70cdd2d5dd2057445da0c29a3f44c49dfe
danieldamian09 <humbertorivero9@gmail.com>
2022-06-09 19:50:03 -0300

    Fix update api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import { errorMessage } from '../components/error.js';
 import { datos } from '../data/datos.js';
 
 
-export const api =  async(city= "Buenos Airea", country = "AR") => {
+export const api =  async(city= "Buenos Aires", country = "AR") => {
   const apiKEY = 'b463dc96882da7c4efdc8dc95527b092'
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKEY}`;
   
